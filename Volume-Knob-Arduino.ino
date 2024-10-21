0eda4924c55f44b6ea2b6579940a46e6e3186696
Leighzer <bwc_nothgiel@live.com>
2024-10-20 23:09:45 -0400

    Change to having volume up and down buttons

--- Volume-Knob-Arduino.ino
+++ Volume-Knob-Arduino.ino
@@ -1,31 +1,31 @@
 #include <HID-Project.h>
 
-int potPin = A0;    // Pin connected to potentiometer
-int potValue = 0;   // Variable to store potentiometer value (0-1023)
-int lastVolume = 0; // Previous volume value
-int volume = 0;     // Variable to store scaled volume (0-255)
+const int buttonUpPin = 5; 
+const int buttonDownPin = 3;
+
+int buttonUpState = 0;
+int buttonDownState = 0;
 
 void setup() {
   Consumer.begin();
-  Serial.begin(9600);  // Initialize serial communication for debugging
+  
+  pinMode(buttonUpPin, INPUT);
+  pinMode(buttonDownPin, INPUT);
 }
 
 void loop() {
-  potValue = analogRead(potPin);            // Read potentiometer value
-  volume = map(potValue, 0, 1023, 0, 255);  // Scale to 0-255 for volume control
-  Serial.println(volume);                   // Print volume for debugging
+  buttonUpState = digitalRead(buttonUpPin);
+  buttonDownState = digitalRead(buttonDownPin);
   
-  // If volume has changed by more than 5 units, simulate key press
-  if (abs(volume - lastVolume) > 4) {
-    if (volume > lastVolume) {
-      Consumer.write(MEDIA_VOLUME_UP);  // Simulate volume up key
-      Consumer.write(MEDIA_VOLUME_UP);  // Simulate volume up key
-    } else if (volume < lastVolume) {
-      Consumer.write(MEDIA_VOLUME_DOWN); // Simulate volume down key
-      Consumer.write(MEDIA_VOLUME_DOWN); // Simulate volume down key
-    }
-    lastVolume = volume;  // Update lastVolume
+  if (buttonUpState == HIGH) {
+    Consumer.write(MEDIA_VOLUME_UP);
+  }
+  
+  if (buttonDownState == HIGH) {
+    Consumer.write(MEDIA_VOLUME_DOWN);
   }
 
-  delay(100);  // Short delay for stability
+  Serial.println("buttonDown " + String(buttonDownState) + " buttonUp " + String(buttonUpState));
+  
+  delay(50);
 }
